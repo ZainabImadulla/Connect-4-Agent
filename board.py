341f0e7e32ac137c80c94afd0fa87eb8fc82d498
ZainabImadulla <zainab.imadulla@icloud.com>
2024-11-27 02:36:23 -0500

    finished board functionality and testing

---
+++ board.py
@@ -0,0 +1,164 @@
+# Board.py
+# Represents a board in a game of connect 4
+class Board:
+    def __init__(self, board=None):
+        if(board == None):
+            self.board = [[0] * 7 for i in range(6)]
+        else:
+            self.board = board
+
+
+    def add_coin(self, column, player):
+        for i in range(5, -1, -1):
+            if not self.has_coin(i, column):
+                self.board[i][column] = player
+                break
+        
+    def move_possible(self, column):
+        if column > 6 or column < 0:
+            return False
+        possible = False
+        for i in range(5, -1, -1):
+            possible = possible or not self.has_coin(i, column)
+        
+        return possible
+
+    def has_coin(self, row, column):
+        if self.board[row][column] == 0:
+            return False
+        return True
+    
+    def check_won(self):
+        return self.check_vertical or self.check_horizontal or self.check_diagonal
+
+
+    def check_vertical(self):
+        for x in range(7):
+            count = 0
+            prev_coin = 0
+            for y in range(6):
+                if self.has_coin(y, x) and count == 0 and self.board[y][x] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[y][x]
+                elif self.has_coin(y, x) and prev_coin == self.board[y][x]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[y][x]
+        return False
+                
+    def check_horizontal(self):
+        for x in range(6):
+            count = 0
+            prev_coin = 0
+            for y in range(7):
+                if self.has_coin(x, y) and count == 0 and self.board[x][y] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[x][y]
+                elif self.has_coin(x, y) and prev_coin == self.board[x][y]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[x][y]
+        return False
+
+    def check_diagonal(self):
+        for f in range(3, 6):
+            count = 0
+            x = 0
+            prev_coin = 0
+            for z in range(f, -1, -1):
+                if self.has_coin(z, x) and count == 0 and self.board[z][x] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[z][x]
+                elif self.has_coin(z, x) and prev_coin == self.board[z][x]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[z][x]
+                x += 1
+            if count >= 4:
+                return True
+        
+        for f in range(1, 4):
+            count = 0
+            y = 5
+            for z in range(f, 6):
+                if self.has_coin(y, z) and count == 0 and self.board[y][z] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[y][z]
+                elif self.has_coin(y, z) and prev_coin == self.board[y][z]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[y][z]
+                y -= 1
+            if count >= 4:
+                return True
+
+
+        for f in range(3, 6):
+            count = 0
+            x = 6
+            prev_coin = 0
+            for z in range(f, -1, -1):
+                if self.has_coin(z, x) and count == 0 and self.board[z][x] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[z][x]
+                elif self.has_coin(z, x) and prev_coin == self.board[z][x]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[z][x]
+                x -= 1
+            if count >= 4:
+                return True
+
+        for f in range(3, 7):
+            count = 0
+            y = 5
+            prev_coin = 0
+            for z in range(f, -1, -1):
+                if self.has_coin(y, z) and count == 0 and self.board[y][z] != prev_coin:
+                    count = 1
+                    prev_coin = self.board[y][z]
+                elif self.has_coin(y, z) and prev_coin == self.board[y][z]:
+                    count += 1
+                    if count == 4:
+                        return True
+                else: 
+                    count = 0
+                prev_coin = self.board[y][z]
+                y -= 1
+            if count >= 4:
+                return True
+        
+        return False
+        
+        
+
+
+       
+
+
+
+
+
+        
+
+
+       
+
+
+
+
---
+++ board_tests.py
@@ -0,0 +1,102 @@
+import unittest
+from board import Board
+
+class BoardTests(unittest.TestCase):
+    board1 = Board()
+    board2 = Board([[0, 0, 0, 0, 0, 0, 2],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 2, 0, 0, 0, 2],
+                    [1, 0, 2, 0, 0, 0, 1],
+                    [1, 0, 2, 0, 0, 0, 2],
+                    [1, 1, 1, 1, 0, 0, 1]])
+
+    board4 = Board([[0, 0, 0, 0, 0, 0, 2],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 2, 0, 0, 0, 2],
+                    [2, 0, 2, 0, 0, 0, 1],
+                    [1, 2, 2, 2, 2, 2, 2],
+                    [1, 2, 1, 1, 2, 2, 1]])
+
+    board5 = Board([[0, 0, 0, 0, 0, 0, 2],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 2, 2, 0, 0, 2],
+                    [2, 0, 2, 2, 0, 0, 1],
+                    [1, 2, 2, 2, 2, 2, 2],
+                    [2, 2, 1, 1, 2, 2, 1]])
+
+    board6 = Board([[0, 0, 0, 0, 0, 0, 2],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 2, 2, 0, 1, 2],
+                    [2, 0, 2, 2, 1, 0, 1],
+                    [1, 0, 2, 1, 2, 2, 2],
+                    [2, 0, 1, 1, 2, 2, 1]])
+
+    board7 = Board([[0, 0, 0, 1, 0, 0, 2],
+                    [0, 0, 1, 0, 1, 0, 1],
+                    [1, 0, 2, 2, 0, 1, 2],
+                    [2, 0, 2, 2, 0, 0, 1],
+                    [1, 0, 2, 1, 2, 2, 2],
+                    [2, 0, 1, 1, 2, 2, 1]])
+
+    board8 = Board([[0, 0, 0, 1, 0, 0, 2],
+                    [0, 0, 1, 0, 0, 0, 1],
+                    [1, 0, 2, 0, 0, 0, 2],
+                    [2, 1, 2, 2, 1, 0, 1],
+                    [1, 2, 1, 0, 0, 2, 2],
+                    [2, 2, 1, 1, 2, 2, 1]])
+
+
+
+    def testMovePossible(self):
+        self.assertFalse(self.board1.move_possible(-1))
+        self.assertTrue(self.board1.move_possible(0))
+        self.assertTrue(self.board2.move_possible(0))
+        self.assertFalse(self.board2.move_possible(6))
+        self.assertTrue(self.board2.move_possible(2))
+
+
+    def testhasCoin(self):
+        self.assertFalse(self.board1.has_coin(0, 0))
+        self.assertTrue(self.board2.has_coin(5, 6))
+        self.assertTrue(self.board2.has_coin(5, 0))
+
+    def testAddCoin(self):
+        board3 = Board([[0, 0, 0, 0, 0, 0, 2],
+                        [0, 0, 1, 0, 0, 0, 1],
+                        [1, 0, 2, 0, 0, 0, 2],
+                        [1, 0, 2, 0, 0, 0, 1],
+                        [1, 0, 2, 0, 0, 0, 2],
+                        [1, 0, 1, 0, 0, 0, 1]])
+        board3.add_coin(0, 1)
+        self.assertTrue(board3.has_coin(1, 0))
+        board3.add_coin(0, 1)
+        self.assertTrue(board3.has_coin(0, 0))
+
+    def testCheckVertical(self):
+        self.assertFalse(self.board1.check_vertical())
+        self.assertTrue(self.board2.check_vertical())
+        self.assertFalse(self.board4.check_vertical())
+
+    def testCheckHorizontak(self):
+        self.assertFalse(self.board1.check_horizontal())
+        self.assertTrue(self.board2.check_horizontal())
+        self.assertTrue(self.board4.check_horizontal())
+
+    def testCheckDiagonal(self):
+        self.assertFalse(self.board1.check_diagonal())
+        self.assertFalse(self.board2.check_diagonal())
+        self.assertTrue(self.board5.check_diagonal())
+        self.assertTrue(self.board6.check_diagonal())
+        self.assertTrue(self.board7.check_diagonal())
+        self.assertTrue(self.board8.check_diagonal())
+
+
+
+
+
+
+def main():
+    unittest.main()
+
+if __name__ == "__main__":
+    main()
